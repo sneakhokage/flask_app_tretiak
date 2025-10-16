28437283651a3b905e65abd09085df2af8f5172d
TretiakMyroslav <myroslav.tretiak.23@pnu.edu.ua>
2025-10-16 16:52:18 +0300

    Initialize flask app and add basic routes

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello, Flask! Your app is working."
+@app.route('/')
+def home():
+    return render_template('resume.html', title='Резюме')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html', title='Контакти')
+
+if __name__ == '__main__':
+    app.run(debug=True)
