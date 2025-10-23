8fbce5fb4543e17835cdbd3d3537750ce2a2881f
TretiakMyroslav <myroslav.tretiak.23@pnu.edu.ua>
2025-10-23 19:52:23 +0300

    add products

--- app/__init__.py
+++ app/__init__.py
@@ -6,3 +6,5 @@ from app import views
 
 from app.users.views import users_bp
 app.register_blueprint(users_bp, url_prefix='/users')
+from .products import products_bp
+app.register_blueprint(products_bp, url_prefix='/products')
