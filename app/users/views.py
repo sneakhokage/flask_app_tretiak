fe247a20ec143920aab7d8eb5f75ec25bcd0841d
TretiakMyroslav <myroslav.tretiak.23@pnu.edu.ua>
2025-10-23 19:21:13 +0300

    added users blueprint with routes, templates and styling. Implemented navigation bar with link to users page

---
+++ app/users/views.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+
+users_bp = Blueprint('users', __name__, template_folder='templates')
+
+@users_bp.route('/hi/<name>')
+def greetings(name):
+    age = request.args.get('age', '')
+    return render_template('users/hi.html', name=name, age=age)
+
+@users_bp.route('/admin')
+def admin():
+    return redirect(url_for('users.greetings', name='Administrator', age=45))
--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
-from . import app
-from flask import render_template, request, redirect, url_for
+from app import app
+from flask import render_template
 
 @app.route('/')
 def home():
