61cb08cf284d7d2e70be6f171b38ce51d35834bc
TretiakMyroslav <myroslav.tretiak.23@pnu.edu.ua>
2025-10-27 16:36:40 +0200

    Add cookie management functionality to profile page

--- app/users/views.py
+++ app/users/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash, session
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session, make_response
 
 users_bp = Blueprint('users', __name__, template_folder='templates')
 
@@ -34,11 +34,50 @@ def logout():
     flash("Ви успішно вийшли з системи.", "info")
     return redirect(url_for('users.login'))
 
-@users_bp.route('/profile')
+@users_bp.route('/profile', methods=['GET', 'POST'])
 def profile():
-    """Сторінка профілю користувача."""
-    if 'username' in session:
-        return render_template('users/profile.html')
-    else:
+    """Сторінка профілю користувача (також обробляє cookies)."""
+    if 'username' not in session:
         flash("Будь ласка, увійдіть, щоб побачити цю сторінку.", "warning")
         return redirect(url_for('users.login'))
+    
+    if request.method == 'POST':
+        response = make_response(redirect(url_for('users.profile')))
+
+        action = request.form.get('action')
+
+        if action == 'add_cookie':
+            key = request.form.get('cookie_key')
+            value = request.form.get('cookie_value')
+            expires_sec = request.form.get('cookie_expires')
+
+            if key and value:
+                max_age = None
+                if expires_sec:
+                    try:
+                        max_age = int(expires_sec)
+                    except ValueError:
+                        pass 
+
+                response.set_cookie(key, value, max_age=max_age)
+                flash(f"Кукі '{key}' успішно додано!", "success")
+
+        elif action == 'delete_cookie':
+            key = request.form.get('delete_key')
+            if key:
+                if key in request.cookies:
+                    response.delete_cookie(key)
+                    flash(f"Кукі '{key}' успішно видалено!", "success")
+                else:
+                    flash(f"Кукі '{key}' не знайдено!", "warning")
+
+        elif action == 'delete_all_cookies':
+            cookies_keys = request.cookies
+            for key in cookies_keys:
+                if key != 'session':
+                    response.delete_cookie(key)
+            flash("Всі кукі (окрім сесії) видалено.", "info")
+
+        return response
+    all_cookies = request.cookies
+    return render_template('users/profile.html', cookies=all_cookies)
