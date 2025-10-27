bf4a84113d11233d6abeeab92d4b7a629fdaf24a
TretiakMyroslav <myroslav.tretiak.23@pnu.edu.ua>
2025-10-27 16:24:27 +0200

    Add user login, logout and profile page

--- app/users/views.py
+++ app/users/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session
 
 users_bp = Blueprint('users', __name__, template_folder='templates')
 
@@ -11,7 +11,34 @@ def greetings(name):
 def admin():
     return redirect(url_for('users.greetings', name='Administrator', age=45))
 
-@users_bp.route('/login', methods=['GET'])
+@users_bp.route('/login', methods=['GET', 'POST'])
 def login():
-    """Обробляє GET-запити для сторінки логіну."""
+    """Обробляє GET (показ форми) та POST (вхід) запити."""
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if username == 'admin' and password == '12345':
+            session['username'] = username
+            flash("Ви успішно увійшли!", "success")
+            return redirect(url_for('users.profile'))
+        else:
+            flash("Неправильне ім'я користувача або пароль. Спробуйте ще раз", "danger")
+            return redirect(url_for('users.login'))
     return render_template('users/login.html')
+
+@users_bp.route('/logout')
+def logout():
+    """Видаляє користувача з сесії."""
+    session.pop('username', None)
+    flash("Ви успішно вийшли з системи.", "info")
+    return redirect(url_for('users.login'))
+
+@users_bp.route('/profile')
+def profile():
+    """Сторінка профілю користувача."""
+    if 'username' in session:
+        return render_template('users/profile.html')
+    else:
+        flash("Будь ласка, увійдіть, щоб побачити цю сторінку.", "warning")
+        return redirect(url_for('users.login'))
