f997e7f1a10eaa89ed23786c566062efcc121f73
TretiakMyroslav <myroslav.tretiak.23@pnu.edu.ua>
2025-10-30 21:08:12 +0200

    Refactor contact page with wtf forms and add page on nav bar

--- app/views.py
+++ app/views.py
@@ -1,10 +1,25 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, flash, redirect, url_for
+from app.forms import ContactForm
 
 @app.route('/')
 def home():
     return render_template('resume.html')
 
-@app.route('/contacts')
+@app.route('/contacts', methods=['GET', 'POST'])
 def contacts():
-    return render_template('contacts.html')
+    form = ContactForm()
+    
+    if form.validate_on_submit():
+
+        name = form.name.data
+        email = form.email.data
+        phone = form.phone.data
+        subject = form.subject.data
+        message = form.message.data
+  
+        flash(f'Дякуємо за звернення, {name}! Ми отримали ваше повідомлення.', 'success')
+ 
+        return redirect(url_for('contacts'))
+
+    return render_template('contacts.html', title='Контакти', form=form)
