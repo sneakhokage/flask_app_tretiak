e0448d4fcd5471c8c36115ee39e28d4cf66da4a6
TretiakMyroslav <myroslav.tretiak.23@pnu.edu.ua>
2025-11-14 12:09:56 +0200

    Migrations for categories and inserting data

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,27 @@
-import os
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import MetaData
 from config import config  
+import os
 
-db = SQLAlchemy()
+from dotenv import load_dotenv
+load_dotenv()
+
+class Base(DeclarativeBase):
+    metadata = MetaData(naming_convention={
+        "ix": 'ix_%(column_0_label)s',
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s"
+    })
+
+db = SQLAlchemy(model_class=Base)
 migrate = Migrate()
 
-def create_app(config_name=os.getenv('FLASK_ENV', 'default')):
+def create_app(config_name: str = os.environ.get("production", "default")) -> Flask:
     """
     Створення екземпляру додатку Flask.
     """
@@ -16,7 +30,7 @@ def create_app(config_name=os.getenv('FLASK_ENV', 'default')):
     app.config.from_object(config[config_name])
 
     db.init_app(app)
-    migrate.init_app(app, db)
+    migrate.init_app(app, db, render_as_batch=True)
 
     from .users import users_bp
     app.register_blueprint(users_bp, url_prefix='/users')
--- app/products/models.py
+++ app/products/models.py
@@ -3,9 +3,33 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import Integer, String, Float, Boolean
 from datetime import datetime
 from sqlalchemy import DateTime, func
+
+
 class Product(db.Model):
     __tablename__ = 'products'
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     price: Mapped[float] = mapped_column(Float, nullable=False)
+    active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False, server_default=func.now())
+
+    category_id: Mapped[int | None] = mapped_column(db.ForeignKey('categories.id'))
+    category: Mapped["Category"] = relationship("Category", back_populates="products")
+
+    def __repr__(self) -> str:
+        return f"<Product {self.name} - ${self.price}>"
+
+
+
+class Category(db.Model):
+    __tablename__ = "categories"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(128), nullable=False, unique=True)
+
+    products: Mapped[list["Product"]] = relationship(
+        "Product",
+        back_populates="category",
+        lazy="select"  
+    )
